46d750ef8235b08dc82b6e7ecdaf4a2e39dbc0a8
Pareesa Kamgar Dayhoff <pareesakd1118@gmail.com>
2024-10-03 14:01:22 -0700

    Add strong params for article create controller, validation for article model and error messaging on article form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,28 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end 
+
+  def new 
+    @article = Article.new
+  end
+
+  def create 
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entry
+    end
+  end
+
+  private ##this is a private method, meaning it helps things work smoothly behind the scenes rather than hold direct logic for a CRUD method. This is an example of strong parameters and what its saying is in the article param you can only submit values for title and body attributes, anything else during form submission will be filtered out essentially. 
+    def article_params
+      params.require(:article).permit(:title, :body)
+    end 
 end
